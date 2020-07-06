3d27d9a6c5bc627ffc4a2ad9d1637d2f61a2f850
Jacob Franz <franzj@fb.com>
2020-07-06 11:08:32 -0500

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Jacob Franz on 7/6/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
